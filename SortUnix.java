a9bf34a9d50c1d026dee2173f3fb02e1f7ef3e02
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-05-10 11:11:46 -0700

    sort on specific col

--- SortUnix.java
+++ SortUnix.java
@@ -5,7 +5,7 @@ public class SortUnix {
 
         System.out.println("Sorting... be patient");
 		//Remove ".exe" for Mac and Linux
-        Runtime.getRuntime().exec("sort -k 2 taxpayers_3M.txt -o taxpayers_3M_sort_answer.txt").waitFor();
+        Runtime.getRuntime().exec("sort -k 2,2 taxpayers_3M.txt -o taxpayers_3M_sort_answer.txt").waitFor();
 
 		System.out.println("Time elapsed (sec) = " + (System.currentTimeMillis() - startTime) / 1000.0);
 	}
